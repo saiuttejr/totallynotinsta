8e06ca4169e67e27ed4f474fb0cbf1a06783692e
Sai-Uttej-R <saiuttej.r@gmail.com>
2025-04-10 21:23:49 +0530

    #1commit - Modified files and updated modules

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the values of email and phone number inputs
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-
-    // Here, you'll send these values to the server to handle registration (we'll implement this later)
-});

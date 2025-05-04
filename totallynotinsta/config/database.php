be92bded553548bdd7e9005086484474f3a6b017
Sai-Uttej-R <saiuttej.r@gmail.com>
2025-05-04 21:54:34 +0530

    Updated database.php

--- totallynotinsta/config/database.php
+++ totallynotinsta/config/database.php
@@ -32,8 +32,8 @@ return [
     'connections' => [
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_DSN'),
-            'database' => env('DB_DATABASE', 'instagram-clone'),
+            'dsn' => env('DB_URI'),
+            'database' => env('DB_DATABASE'),
         ],
         'sqlite' => [
             'driver' => 'sqlite',
